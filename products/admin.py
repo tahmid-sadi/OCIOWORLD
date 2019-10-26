252f24c70852a612cf4ad4b28082c77e43ef51d0
tahmid-sadi <tahbaquee@gmail.com>
2019-10-27 01:09:33 +0600

    Models were created and Admin page checked.

---
+++ products/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Chair, Feature
+
+
+admin.site.register(Chair)
+admin.site.register(Feature)
+
---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Chair(models.Model):
+    model_nummber = models.CharField(max_length=20)
+    model_image = models.ImageField(upload_to='images/')
+
+
+class Feature(models.Model):
+    feature_image = models.ImageField(upload_to='images/')
+    chair = models.ForeignKey(Chair, on_delete=models.CASCADE)
+
