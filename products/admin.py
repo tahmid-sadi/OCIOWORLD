deeb4f2acf99804e5e9a18e05d296ff269faeac6
tahmid-sadi <tahbaquee@gmail.com>
2019-10-27 12:21:35 +0600

    Chair objects created and displayed in Admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Chair, Feature
 
 
-admin.site.register(Chair)
-admin.site.register(Feature)
+class ChairAdmin(admin.ModelAdmin):
+    list_display = ('model_number', 'model_image')
+
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ('feature_image', 'chair')
+
+
+admin.site.register(Chair, ChairAdmin)
+admin.site.register(Feature, FeatureAdmin)
 
--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Chair(models.Model):
-    model_nummber = models.CharField(max_length=20)
+    model_number = models.CharField(max_length=20)
     model_image = models.ImageField(upload_to='images/')
 
 
