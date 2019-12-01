0310a5943aee51666d2682ab1722c719a9c9a1f5
tahmid-sadi <tahbaquee@gmail.com>
2019-12-01 19:50:02 +0600

    CSS added and worked which is good news.

--- products/models.py
+++ products/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class Chair(models.Model):
     model_number = models.CharField(max_length=20)
     model_image = models.ImageField(upload_to='images/')
+    model_description = models.TextField(blank=True)
 
     def __str__(self):
         return self.model_number
-
+1
 
 class Feature(models.Model):
     feature_image = models.ImageField(upload_to='images/')
